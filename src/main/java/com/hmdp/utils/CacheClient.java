1339b651f96b02f11dece44f060f68b7380bb453
... <37700859+453594848@users.noreply.github.com>
2024-03-20 23:13:56 +0800

    完善商户查询功能

--- src/main/java/com/hmdp/utils/CacheClient.java
+++ src/main/java/com/hmdp/utils/CacheClient.java
@@ -1,12 +1,20 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
+import com.hmdp.entity.Shop;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
 
 @Component
 @Slf4j
@@ -28,5 +36,71 @@ public class CacheClient {
         //号入Redis
         stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(redisData));
     }
+
+    public <R, ID> R queryWithPassThrough(String keyPrefix, ID id, Class<R> type, Function<ID, R> dbFallback
+            , Long time, TimeUnit unit) {
+        String key = keyPrefix + id;
+        String Json = stringRedisTemplate.opsForValue().get(key);
+        if (StrUtil.isNotBlank(Json)) {
+            return JSONUtil.toBean(Json, type);
+        }
+        if (Json != null) {
+            return null;
+        }
+        R r = dbFallback.apply(id);
+        if (r == null) {
+            stringRedisTemplate.opsForValue().set(key, "", RedisConstants.CACHE_NULL_TTL, TimeUnit.MINUTES);
+            return null;
+        }
+        this.set(key, r, time, unit);
+        return r;
+
+    }
+
+    public <R, ID> R queryWithLogicalExpire(String keyPrefix, ID id, Class<R> type, Function<ID, R> dbFallback
+            , Long time, TimeUnit unit) {
+        String key = keyPrefix + id;
+        String Json = stringRedisTemplate.opsForValue().get(key);
+        if (StrUtil.isBlank(Json)) {
+            return null;
+        }
+        //命中，先吧json反序列化
+        RedisData redisData = JSONUtil.toBean(Json, RedisData.class);
+        R r = JSONUtil.toBean((JSONObject) redisData.getData(), type);
+        LocalDateTime expireTime = redisData.getExpireTime();
+        if (expireTime.isAfter(LocalDateTime.now())) {
+            return r;
+        }
+        String lockKey = "lock:shop:" + id;
+        boolean lock = tryLock(lockKey);
+        if (lock) {
+            CACHE_REBUILD_EXECUTOR.submit(
+                    () -> {
+                        try {
+                            R r1 = dbFallback.apply(id);
+                            stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(r1), RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
+                            return r1;
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        } finally {
+                            unLock(lockKey);
+                        }
+                    });
+        }
+        return null;
+    }
+
+    private static final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(10);
+
+    private boolean tryLock(String key) {
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", RedisConstants.LOCK_SHOP_TTL + 2L, TimeUnit.MINUTES);
+        return BooleanUtil.isTrue(flag);
+    }
+
+    private void unLock(String key) {
+        stringRedisTemplate.delete(key);
+    }
+
+
 }
 
